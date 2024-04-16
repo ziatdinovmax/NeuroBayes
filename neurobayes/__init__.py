e1073d7cb52cc4f79f4536c17065d27482e9b33e
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-04-16 07:11:44 +0000

    Update imports

--- neurobayes/__init__.py
+++ neurobayes/__init__.py
@@ -2,5 +2,7 @@ from .gp import GP
 from .dkl import DKL
 from .vidkl import VIDKL
 from .bnn import BNN
+from . import kernels
+from . import priors
 
-__all__ = ["GP", "DKL", "VIDKL", "BNN"]
+__all__ = ["GP", "DKL", "VIDKL", "BNN", "kernels", "priors"]
