055a442e0af5aeb276b3f8626467c822e90f2aeb
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-05-20 23:19:58 -0700

    Update __init__.py

--- neurobayes/__init__.py
+++ neurobayes/__init__.py
@@ -3,6 +3,7 @@ from .dkl import DKL
 from .vidkl import VIDKL
 from .bnn import BNN
 from .hskbnn import HeteroskedasticBNN
+from .uibnn import UncertainInputBNN
 from . import kernels
 from . import priors
 
