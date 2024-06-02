32b3a570ee5f830a9d7012fe020291095269c380
maxim.ziatdinov <maxim.ziatdinov@pnnl.gov>
2024-06-02 13:09:27 -0700

    Update imports

--- neurobayes/__init__.py
+++ neurobayes/__init__.py
@@ -4,6 +4,7 @@ from .vidkl import VIDKL
 from .bnn import BNN
 from .hskbnn import HeteroskedasticBNN
 from .uibnn import UncertainInputBNN
+from .uibnn2 import UncertainInputBNN2
 from .bnnlvm import BNNLVM
 from . import kernels
 from . import priors
