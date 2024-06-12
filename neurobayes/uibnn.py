86a01d09a103266ca3e96b1f3eca8664b202d736
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2024-06-12 21:14:32 +0000

    Update input sampling

--- neurobayes/uibnn.py
+++ neurobayes/uibnn.py
@@ -45,16 +45,13 @@ class UncertainInputBNN(BNN):
         if self.input_noise_prior is not None:
             sigma_x_dist = self.input_noise_prior
         else:
-            sigma_x_dist = dist.HalfNormal(.1 * jnp.ones(n_features))
-        # Sample variances independently for each feature dimension
+            sigma_x_dist = dist.HalfNormal(.1)
         with numpyro.plate("feature_variance_plate", n_features):
             sigma_x = numpyro.sample("sigma_x", sigma_x_dist)
-            # Sample input data using the sampled variances
-            with numpyro.plate("X_prime_plate", n_samples, dim=-2):
-                X_prime = numpyro.sample("X_prime", dist.Normal(X, sigma_x))
+        # Sample input data using the sampled variances
+        X_prime = numpyro.sample("X_prime", dist.Normal(X, sigma_x))
         return X_prime
     
-
     def sample_single_posterior_predictive(self, rng_key, X_new, params, n_draws):
         sigma_x = params['sigma_x']
         X_new_prime = dist.Normal(X_new, sigma_x).sample(rng_key)
