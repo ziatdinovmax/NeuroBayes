281fa911e045786f238ce562b064be8e26c49dba
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2024-06-21 19:19:30 +0000

    Add flask mlp

--- neurobayes/nn.py
+++ neurobayes/nn.py
@@ -1,6 +1,7 @@
-from typing import Callable, Dict, List, Tuple
+from typing import Callable, Dict, List, Tuple, Sequence
 import jax
 import jax.numpy as jnp
+import flax.linen as nn
 
 
 def get_mlp(hidden_dim: List[int], activation: str = 'tanh', name: str = "main"
@@ -39,3 +40,27 @@ def get_heteroskedastic_mlp(hidden_dim: List[int], activation: str = 'tanh'
         return mean, variance
     return mlp
 
+
+class FlaxMLP(nn.Module):
+    # Define the structure of the network
+    hidden_dims: Sequence[int]  # List of hidden layer sizes
+    output_dim: int             # Number of units in the output layer
+    activation: str = 'tanh'    # Type of activation function, default is 'tanh'
+
+    @nn.compact
+    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
+        """
+        Forward pass of the MLP.
+        """
+        # Set the activation function based on the input parameter
+        activation_fn = nn.tanh if self.activation == 'tanh' else nn.silu
+
+        # Build the hidden layers
+        for i, hidden_dim in enumerate(self.hidden_dims):
+            x = nn.Dense(features=hidden_dim, name=f"Dense{i}")(x)
+            x = activation_fn(x)  # Apply activation function
+
+        # Output layer, no activation function applied here
+        x = nn.Dense(features=self.output_dim, name=f"Dense{len(self.hidden_dims)}")(x) 
+
+        return x
