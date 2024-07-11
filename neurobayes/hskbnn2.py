66851dcf2f5667a276cf2812d1cc5fe1f9adaf49
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-07-11 19:04:00 +0000

    Update variance model-absed hskbnn to flax bnn

--- neurobayes/hskbnn2.py
+++ neurobayes/hskbnn2.py
@@ -1,12 +1,13 @@
-from typing import List, Callable
-import jax
+from typing import List, Callable, Optional
 import jax.numpy as jnp
+import jax.random as jra
 import numpyro
 import numpyro.distributions as dist
+from numpyro.contrib.module import random_flax_module
 
 from .bnn import BNN
-from .nn import get_mlp
-from .priors import get_mlp_prior
+from .nn import FlaxMLP
+from .utils import put_on_device
 
 
 class HeteroskedasticBNN2(BNN):
@@ -23,35 +24,32 @@ class HeteroskedasticBNN2(BNN):
 
         # Override the MLP functions with heteroskedastic versions
         hdim = hidden_dim if hidden_dim is not None else [32, 16, 8]
-        self.nn = get_mlp(hdim, activation)
-        self.nn_prior = get_mlp_prior(input_dim, output_dim, hdim)
+        self.nn = FlaxMLP(hdim, output_dim, activation)
         self.variance_model = variance_model
         self.variance_model_prior = variance_model_prior
 
     def model(self, X: jnp.ndarray, y: jnp.ndarray = None, **kwargs) -> None:
         """BNN probabilistic model"""
+        
+        net = random_flax_module(
+            "nn", self.nn, input_shape=(1, self.input_dim),
+            prior=(lambda name, shape: dist.Cauchy() if name == "bias" else dist.Normal()))
 
-        # Sample NN parameters
-        nn_params = self.nn_prior()
         # Pass inputs through a NN with the sampled parameters
-        mu = self.nn(X, nn_params)
+        mu = numpyro.deterministic("mu", net(X))
 
         # Sample noise variance according to the provided model
         var_params = self.variance_model_prior()
-        sig = self.variance_model(X, var_params)
+        sig = numpyro.deterministic("sig", self.variance_model(X, var_params))
 
         # Score against the observed data points
         numpyro.sample("y", dist.Normal(mu, sig), obs=y)
 
-    def sample_single_posterior_predictive(self, rng_key, X_new, params, n_draws):
-        loc = self.nn(X_new, params)
-        sigma = self.variance_model(X_new, params)
-        sample = dist.Normal(loc, sigma).sample(rng_key, (n_draws,)).mean(0)
-        return loc, sample
-
-    def predict_noise(self, X_new: jnp.ndarray) -> jnp.ndarray:
+    def predict_noise(self, X_new: jnp.ndarray,
+                      device: Optional[str] = None) -> jnp.ndarray:
         X_new = self.set_data(X_new)
-        samples = self.get_samples(chain_dim=False)
-        predictive = jax.vmap(lambda params: self.variance_model(X_new, params))
-        sigma = predictive(samples)
-        return sigma.mean(0)
+        samples = self.get_samples()
+        X_new, samples = put_on_device(device, X_new, samples)
+        pred = self.sample_from_posterior(
+            jra.PRNGKey(0), X_new, samples, return_sites=['sig'])
+        return pred['sig'].mean(0)
