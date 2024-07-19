a33bbb9170b229e77c6a9d635196885d50485856
Maxim Ziatdinov <34245227+ziatdinovmax@users.noreply.github.com>
2024-07-19 23:37:52 +0000

    First draft of multi-task bnn

--- neurobayes/__init__.py
+++ neurobayes/__init__.py
@@ -8,6 +8,7 @@ from .hskbnn import HeteroskedasticBNN
 from .hskbnn2 import HeteroskedasticBNN2
 from .uibnn import UncertainInputBNN
 from .detnn import DeterministicNN
+from .mtbnn import MultitaskBNN
 from . import kernels
 from . import priors
 from . import utils
