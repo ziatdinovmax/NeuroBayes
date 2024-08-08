303e5bdc8513689e2e70f46305cf9911427d34f2
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2024-08-08 14:51:24 -0700

    Incorporate training of deterministic NN into fit()

--- neurobayes/pdkl.py
+++ neurobayes/pdkl.py
@@ -9,6 +9,7 @@ from numpyro.contrib.module import random_flax_module
 from .dkl import DKL
 from .priors import GPPriors
 from .utils import split_mlp
+from .detnn import DeterministicNN
 
 kernel_fn_type = Callable[[jnp.ndarray, jnp.ndarray, Dict[str, jnp.ndarray], jnp.ndarray],  jnp.ndarray]
 
@@ -22,14 +23,20 @@ class PartialDKL(DKL):
                  latent_dim: int,
                  base_kernel: kernel_fn_type,
                  deterministic_nn: Type[flax.linen.Module],
-                 deterministic_weights: Dict[str, jnp.ndarray],
+                 deterministic_weights: Optional[Dict[str, jnp.ndarray]] = None,
+                 input_dim: int = None,
                  priors: Optional[GPPriors] = None,
                  jitter: float = 1e-6,
                  ) -> None:
         super(PartialDKL, self).__init__(None, latent_dim, base_kernel, priors, jitter)
-        (self.truncated_nn, self.truncated_params,
-         self.nn) = split_mlp(
-             deterministic_nn, deterministic_weights, latent_dim)[:-1]
+        if deterministic_weights:
+            (self.truncated_mlp, self.truncated_params,
+             self.last_layer_mlp) = split_mlp(
+                 deterministic_nn, deterministic_weights, latent_dim)[:-1]
+        else:
+            self.untrained_deterministic_nn = deterministic_nn
+            if not input_dim:
+                raise ValueError("Please provide input data dimensions or pre-trained model parameters")  
 
     def model(self, X: jnp.ndarray, y: jnp.ndarray = None, **kwargs) -> None:
         """BNN probabilistic model"""
@@ -55,6 +62,42 @@ class PartialDKL(DKL):
             dist.MultivariateNormal(loc=f_loc, covariance_matrix=k),
             obs=y,
         )
+
+    def fit(self, X: jnp.ndarray, y: jnp.ndarray,
+            num_warmup: int = 2000, num_samples: int = 2000,
+            num_chains: int = 1, chain_method: str = 'sequential',
+            sgd_epochs: Optional[int] = None,
+            progress_bar: bool = True, device: str = None,
+            rng_key: Optional[jnp.array] = None,
+            ) -> None:
+        """
+        Run HMC to infer parameters of the DKL
+
+        Args:
+            X: 2D feature vector
+            y: 1D target vector
+            num_warmup: number of HMC warmup states
+            num_samples: number of HMC samples
+            num_chains: number of HMC chains
+            sgd_epochs:
+                number of SGD training epochs for deterministic NN
+                (if trained weights are not provided at the initialization stage)
+            chain_method: 'sequential', 'parallel' or 'vectorized'
+            progress_bar: show progress bar
+            device:
+                The device (e.g. "cpu" or "gpu") perform computation on ('cpu', 'gpu'). If None, computation
+                is performed on the JAX default device.
+            rng_key: random number generator key
+        """
+        X, y = self.set_data(X, y)
+        if hasattr(self, "untrained_deterministic_nn"):
+            print("Training deterministic NN...")
+            det_nn = DeterministicNN(self.untrained_deterministic_nn, self.input_dim)
+            det_nn.train(X, y, 500 if sgd_epochs is None else sgd_epochs)
+            (self.truncated_mlp, self.truncated_params,
+            self.last_layer_mlp) = split_mlp(
+                det_nn.model, det_nn.params)[:-1]
+        super().fit(X, y, num_warmup, num_samples, num_chains, chain_method, progress_bar, device, rng_key)
 
     def compute_gp_posterior(self,
                              X_new: jnp.ndarray,
