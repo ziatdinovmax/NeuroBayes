6fadbcdb9b4d10068002729f594b8a9353070a60
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2024-08-13 11:56:38 -0700

    Simplify 2-head NN for heteroskedastic BNN

--- neurobayes/nn.py
+++ neurobayes/nn.py
@@ -32,37 +32,16 @@ class FlaxMLP(nn.Module):
         return x
 
 
-class FlaxMLP2Head(nn.Module):
-    # Define the structure of the network
-    hidden_dims: Sequence[int]  # List of hidden layer sizes
-    output_dim: int             # Number of units in each output head (mean and variance)
-    activation: str = 'tanh'    # Type of activation function, default is 'tanh'
-
+class FlaxMLP2Head(FlaxMLP):
     @nn.compact
-    def __call__(self, x: jnp.ndarray) -> jnp.ndarray:
-        """
-        Forward pass of the MLP with two heads: one for mean and one for variance.
-        """
-        # Set the activation function based on the input parameter
-        activation_fn = nn.tanh if self.activation == 'tanh' else nn.silu
-
-        # Build the hidden layers
-        for i, hidden_dim in enumerate(self.hidden_dims):
-            x = nn.Dense(features=hidden_dim, name=f"Dense{i}")(x)
-            x = activation_fn(x)  # Apply activation function
-
+    def __call__(self, x: jnp.ndarray) -> tuple[jnp.ndarray, jnp.ndarray]:
+        x = super().__call__(x)
         if self.output_dim:
-            # Two separate output layers, one for mean and one for variance
-            # Mean head
             mean = nn.Dense(features=self.output_dim, name="MeanHead")(x)
-            
-            # Variance head
             variance = nn.Dense(features=self.output_dim, name="VarianceHead")(x)
-            variance = nn.softplus(variance)  # ensure the output is positive
-
+            variance = nn.softplus(variance)
             return mean, variance
-        else:
-            return x
+        return x
 
     
 class Embedding(nn.Module):
