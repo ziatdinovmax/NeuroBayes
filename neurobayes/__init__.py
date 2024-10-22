dc5c26abd3a1ddef585b7c269789d95c93ed7e36
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2024-10-22 12:14:30 -0700

    Update imports

--- neurobayes/__init__.py
+++ neurobayes/__init__.py
@@ -4,6 +4,7 @@ from .models.dkl import DKL
 from .models.vidkl import VIDKL
 from .models.bnn import BNN
 from .models.partial_bnn import PartialBNN
+from .models.partial_bnn2 import PartialBNN2
 from .models.partial_dkl import PartialDKL
 from .models.bnn_heteroskedastic import HeteroskedasticBNN
 from .models.bnn_heteroskedastic_model import VarianceModelHeteroskedasticBNN
