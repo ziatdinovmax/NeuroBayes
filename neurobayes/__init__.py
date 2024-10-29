31c9ef64b8747f3e573f2434894ee3c5487ea7f6
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2024-10-29 14:43:30 -0700

    Clean up

--- neurobayes/__init__.py
+++ neurobayes/__init__.py
@@ -4,7 +4,6 @@ from .models.dkl import DKL
 from .models.vidkl import VIDKL
 from .models.bnn import BNN
 from .models.partial_bnn import PartialBNN
-from .models.partial_dkl import PartialDKL
 from .models.bnn_heteroskedastic import HeteroskedasticBNN
 from .models.bnn_heteroskedastic_model import VarianceModelHeteroskedasticBNN
 from .models.partial_bnn_heteroskedastic import HeteroskedasticPartialBNN
@@ -23,7 +22,6 @@ __all__ = [
     "VIDKL",
     "BNN",
     "PartialBNN",
-    "PartialDKL",
     "HeteroskedasticBNN",
     "VarianceModelHeteroskedasticBNN",
     "HeteroskedasticPartialBNN",
