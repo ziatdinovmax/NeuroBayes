02308c4ec7413c1cb9ef0056226b8192e1029984
Maxim Ziatdinov <ziatdinovmax@gmail.com>
2024-11-25 16:07:55 -0800

    update heteroskedastic BNN

--- neurobayes/models/bnn_heteroskedastic_model.py
+++ neurobayes/models/bnn_heteroskedastic_model.py
@@ -26,14 +26,14 @@ class VarianceModelHeteroskedasticBNN(HeteroskedasticBNN):
         self.variance_model = variance_model
         self.variance_model_prior = variance_model_prior
 
-    def model(self, X: jnp.ndarray, y: jnp.ndarray = None, **kwargs) -> None:
+    def model(self, X: jnp.ndarray, y: jnp.ndarray = None, priors_sigma: float = 1.0, **kwargs) -> None:
         """Heteroskedastic BNN model"""
 
         input_shape = X.shape[1:] if X.ndim > 2 else (X.shape[-1],)
 
         net = random_flax_module(
-            "nn", self.nn, input_shape=(1, input_shape),
-            prior=(lambda name, shape:  dist.Normal(0, 1)))
+            "nn", self.nn, input_shape=(1, *input_shape),
+            prior=(lambda name, shape:  dist.Normal(0, priors_sigma)))
 
         # Pass inputs through a NN with the sampled parameters
         mu = numpyro.deterministic("mu", net(X))
@@ -41,6 +41,6 @@ class VarianceModelHeteroskedasticBNN(HeteroskedasticBNN):
         # Sample noise variance according to the provided model
         var_params = self.variance_model_prior()
         sig = numpyro.deterministic("sig", self.variance_model(X, var_params))
-
+        
         # Score against the observed data points
         numpyro.sample("y", dist.Normal(mu, sig), obs=y)
